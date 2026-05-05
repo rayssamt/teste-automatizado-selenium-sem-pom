a4e269d55297da34dddda9be8759a092032a9734
rayssamt <rayssamt60@gmail.com>
2026-05-05 13:24:42 -0300

    complementando teste de adicao de produto ao carrinho

--- tests/test_adicionar_produtos_carrinho.py
+++ tests/test_adicionar_produtos_carrinho.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -18,9 +20,14 @@ driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
 
-#add ao carrinho diretamente da paga inicial
+#add ao carrinho diretamente da pagina inicial
 driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+
+#add ao carrinho clicando no produto, pela página do produto
+driver.find_element(By.ID, "item_0_title_link").click()
+driver.find_element(By.ID, "add-to-cart").click()
 
 #verificar se foi adicionado ao carrinho
 driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
-assert driver.find_element(By.XPATH, "//div[@class='inventory_item_name' and text()='Sauce Labs Backpack']").is_displayed()
+assert driver.find_element(By.XPATH, "//div[@class='inventory_item_name' and contains(text(), 'Sauce Labs Backpack')]").is_displayed()
+assert driver.find_element(By.XPATH, "//div[@class='inventory_item_name' and contains(text(), 'Sauce Labs Bike Light')]").is_displayed()
