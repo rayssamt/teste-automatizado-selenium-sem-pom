9ca37f05a7fef54724f667562b8dc44249923b48
rayssamt <rayssamt60@gmail.com>
2026-05-05 12:47:54 -0300

    adicionando teste de adição de produto ao carrinho

---
+++ tests/test_adicionar_produtos_carrinho.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.maximize_window()
+driver.get("https://www.saucedemo.com/")
+
+#por o user
+#por a senha
+#clickar no botão login
+#add produto ao carrinho
+#entrar no carrinho
+#verificar se foi adicionado
+
+#login
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+
+#add ao carrinho diretamente da paga inicial
+driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+
+#verificar se foi adicionado ao carrinho
+driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
+assert driver.find_element(By.XPATH, "//div[@class='inventory_item_name' and text()='Sauce Labs Backpack']").is_displayed()
